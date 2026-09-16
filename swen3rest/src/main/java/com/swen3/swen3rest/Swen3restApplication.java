2465d2b85aa49dac737afc4cb1e3afcf1eac5be4
Mateo Lamthi <163135115+lamthimateo@users.noreply.github.com>
2026-09-16 17:38:08 +0200

    Add Spring Boot skeleton (swen3rest)

---
+++ swen3rest/src/main/java/com/swen3/swen3rest/Swen3restApplication.java
@@ -0,0 +1,13 @@
+package com.swen3.swen3rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swen3restApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swen3restApplication.class, args);
+	}
+
+}
---
+++ swen3rest/src/test/java/com/swen3/swen3rest/Swen3restApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swen3.swen3rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swen3restApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
